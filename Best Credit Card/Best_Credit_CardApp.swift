6bec502ebb4c3fb25a25ff1a9f8bfcaa88eaead2
Scott V <scottvogelgesang@gmail.com>
2026-02-25 12:51:33 -0800

    Initial Commit

---
+++ Best Credit Card/Best_Credit_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Best_Credit_CardApp.swift
+//  Best Credit Card
+//
+//  Created by Scott Vogelgesang on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Best_Credit_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
